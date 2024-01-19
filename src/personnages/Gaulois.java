5e3671df206a6c32eaa55e3d7c29f71a64098ff8
tolotra1209 <tolotra1209@gmail.com>
2024-01-19 08:26:01 +0100

    src

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,15 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+}
